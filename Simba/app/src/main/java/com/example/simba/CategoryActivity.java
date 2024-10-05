8331503fde98972949795189d80b72e7def81cac
novakvova <novakvova@gmail.com>
2024-10-05 17:47:15 +0300

    Good working app

---
+++ Simba/app/src/main/java/com/example/simba/CategoryActivity.java
@@ -0,0 +1,32 @@
+package com.example.simba;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_category);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+//        String url = "https://content2.rozetka.com.ua/goods/images/big/472156379.jpg";
+        String url = "http://malyska123.somee.com/images/noimage.jpg";
+        ImageView imgTest = findViewById(R.id.imgTest);
+        Glide.with(this).load(url).into(imgTest);
+    }
+}

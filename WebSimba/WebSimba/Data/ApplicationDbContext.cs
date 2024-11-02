c739628c50475910d588b6cb29699e992e329e7f
novakvova <novakvova@gmail>
2024-11-02 17:13:08 +0200

    Identity Add Migration

--- WebSimba/WebSimba/Data/ApplicationDbContext.cs
+++ WebSimba/WebSimba/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebSimba.Data.Entities;
+using WebSimba.Data.Entities.Identity;
 
 namespace WebSimba.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<UserEntity, RoleEntity, int>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -13,5 +15,22 @@ namespace WebSimba.Data
         public DbSet<CategoryEntity> Categories { get; set; }
         public DbSet<ProductEntity> Products { get; set; }
         public DbSet<ProductImageEntity> ProductImages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<UserRoleEntity>(ur =>
+            {
+                ur.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(r => r.RoleId)
+                    .IsRequired();
+
+                ur.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(u => u.UserId)
+                    .IsRequired();
+            });
+        }
     }
 }
---
+++ WebSimba/WebSimba/Data/Entities/Identity/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebSimba.Data.Entities.Identity
+{
+    public class UserEntity : IdentityUser<int>
+    {
+        [StringLength(255)]
+        public string? Image { get; set; }
+        [StringLength(100)]
+        public string? LastName { get; set; }
+        [StringLength(100)]
+        public string? FirstName { get; set; }
+        public virtual ICollection<UserRoleEntity>? UserRoles { get; set; }
+    }
+}
